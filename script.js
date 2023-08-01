f45749e3de6f664fc19aa4564db02fff96fb7d99
Everton F Silva <evertonfernandes097@gmail.com>
2023-08-01 15:58:54 -0300

    Criado score do jogo.

--- script.js
+++ script.js
@@ -11,3 +11,115 @@ const navlinks = document.querySelectorAll('nav a').forEach(link => {
         link.classList.add('acrive');
     }
 })
+
+const valueTime01 = document.getElementById('valueTime01');
+const plusButtonTime01 = document.getElementById('plusTime01');
+const minusButtonTime01 = document.getElementById('minusTime01');
+const bola01 = document.getElementById('bolavolei01').style.opacity;
+
+const updateValueTime01 = () => {
+    if(countTime01<=9){
+        valueTime01.innerHTML = '0'+countTime01;
+    }else if(countTime01>9){
+        valueTime01.innerHTML = countTime01;
+    }
+};
+const updateBola01 = () => {
+    document.getElementById('bolavolei01').style.opacity = "1";
+    document.getElementById('bolavolei02').style.opacity = "0";
+};
+const updateBola02 = () => {
+    document.getElementById('bolavolei01').style.opacity = "0";
+    document.getElementById('bolavolei02').style.opacity = "1";
+};
+
+const valueTime02 = document.getElementById('valueTime02');
+const plusButtonTime02 = document.getElementById('plusTime02');
+const minusButtonTime02 = document.getElementById('minusTime02');
+const updateValueTime02 = () => {
+    if(countTime02<=9){
+        valueTime02.innerHTML = '0'+countTime02;
+    }else if(countTime02>9){
+        valueTime02.innerHTML = countTime02;
+    }
+};
+
+let countTime01 = 0;
+let intervalIDTime01 = 0;
+plusButtonTime01.addEventListener('click', ()=> {
+    if(countTime01>=0 && countTime01<40){
+        intervalIDTime01 = countTime01 += 1;
+        updateValueTime01();
+        updateBola01();
+    }
+});
+plusButtonTime01.addEventListener('mousedown', ()=> {
+    if(countTime01>=0 && countTime01<40){
+        intervalIDTime01 = setInterval(()=> {
+            countTime01 += 1;
+            updateValueTime01();
+            updateBola01();
+            if(countTime01==40){
+                clearInterval(intervalIDTime01);
+            }
+        },100);
+    }
+});
+minusButtonTime01.addEventListener('click', ()=> {
+    if(countTime01>0 && countTime01<=40){
+        intervalIDTime01 = countTime01 -= 1;
+    updateValueTime01();
+    } 
+});
+minusButtonTime01.addEventListener('mousedown', ()=> {
+    if(countTime01>0 && countTime01<=40){
+        intervalIDTime01 = setInterval(()=> {
+            countTime01 -= 1;
+        updateValueTime01();
+        if(countTime01==40 || countTime01==0){
+            clearInterval(intervalIDTime01);
+        }
+    },100);
+    }
+});
+document.addEventListener('mouseup', () => clearInterval(intervalIDTime01));
+
+let countTime02 = 0;
+let intervalIDTime02 = 0;
+plusButtonTime02.addEventListener('click', ()=> {
+    if(countTime02>=0 && countTime02<40){
+        intervalIDTime02 = countTime02 += 1;
+        updateValueTime02();
+        updateBola02();
+    }
+});
+plusButtonTime02.addEventListener('mousedown', ()=> {
+    if(countTime02>=0 && countTime02<40){
+        intervalIDTime02 = setInterval(()=> {
+            countTime02 += 1;
+            updateValueTime02();
+            updateBola02();
+            if(countTime02==40){
+                clearInterval(intervalIDTime02);
+            }
+        },100);
+    }
+});
+minusButtonTime02.addEventListener('click', ()=> {
+    if(countTime02>0 && countTime02<=40){
+        intervalIDTime02 = countTime02 -= 1;
+    updateValueTime02();
+    } 
+});
+minusButtonTime02.addEventListener('mousedown', ()=> {
+    if(countTime02>0 && countTime02<=40){
+        intervalIDTime02 = setInterval(()=> {
+            countTime02 -= 1;
+        updateValueTime02();
+        if(countTime02==40 || countTime02==0){
+            clearInterval(intervalIDTime02);
+        }
+    },100);
+    }
+});
+document.addEventListener('mouseup', () => clearInterval(intervalIDTime02));
